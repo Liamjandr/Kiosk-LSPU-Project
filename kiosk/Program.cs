44171aaf5cfbb3cbcf811776d4655b42266b79ff
ver505 <layagjohnver2@gmail.com>
2025-11-26 15:41:07 +0800

    Update Program.cs

--- kiosk/Program.cs
+++ kiosk/Program.cs
@@ -17,6 +17,7 @@ namespace kiosk
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Main());
+            //greger
         }
     }
 }
