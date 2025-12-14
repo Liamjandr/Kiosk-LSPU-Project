92bf7b5b2d68fc759b19cac4c64d61ea68dcd3ec
Liam Jan de Roxas <liamdredu@gmail.com>
2025-12-14 10:09:07 +0800

    Update AddPurchase.cs

--- kiosk/AddPurchase.cs
+++ kiosk/AddPurchase.cs
@@ -1,10 +1,12 @@
-﻿using Paymongo.Sharp.Features.Checkouts.Contracts;
+﻿using MySql.Data.MySqlClient;
+using Paymongo.Sharp.Features.Checkouts.Contracts;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -15,6 +17,7 @@ namespace kiosk
     public partial class addPurchase : UserControl
     {
 
+        public string mycon = "datasource=localhost;Database=dbkiosk;username=root;convert zero datetime=true";
         string isPaid { get; set; }
         string isClaimed { get; set; }
         List<HistoryDB> receiptItems { get; set; }
@@ -42,7 +45,7 @@ namespace kiosk
                 Payment.Text = "UNPAID";
                 Payment.FillColor = Color.Red;
             }
-            if (history.Items[0].isClaimed == "false")
+            if (history.Items[0].isClaimed == "true")
             {
                 Claim.Text = "Claimed";
                 Claim.FillColor = Color.Green;
@@ -58,6 +61,7 @@ namespace kiosk
         {
             if(isPaid == "false") isPaid = "true";
             else isPaid = "false";
+            changeStatePaid(ReceiptID.Text, isPaid);
 
             if (isPaid == "true")
             {
@@ -75,6 +79,7 @@ namespace kiosk
         {
             if(isClaimed == "false") isClaimed = "true";
             else isClaimed = "false";
+            changeStateClaim(ReceiptID.Text, isClaimed);
             if (isClaimed == "true")
             {
                 Claim.Text = "Claimed";
@@ -93,6 +98,66 @@ namespace kiosk
             var main = this.FindForm() as Main;
             if (main == null) return;
             main.showReceiptModal(receiptItems);
+        }
+
+        private void changeStatePaid(string id, string paid)
+        {
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(mycon))
+                {
+                    conn.Open();
+
+                    string query = @"
+                        UPDATE tbhistory
+                        SET isPaid = @paid
+                        WHERE ReceiptID = @receiptID";
+
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@paid", paid);
+                        cmd.Parameters.AddWithValue("@receiptID", id);
+
+                        int rows = cmd.ExecuteNonQuery();
+                        //MessageBox.Show("Updated rows: " + rows);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Invalid Input: " + ex.Message);
+                return;
+            }
+        }
+
+        private void changeStateClaim(string id, string claim)
+        {
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(mycon))
+                {
+                    conn.Open();
+
+                    string query = @"
+                        UPDATE tbhistory
+                        SET isClaimed = @claim
+                        WHERE ReceiptID = @receiptID";
+
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@claim", claim);
+                        cmd.Parameters.AddWithValue("@receiptID", id);
+
+                        int rows = cmd.ExecuteNonQuery();
+                        //MessageBox.Show("Updated rows: " + rows);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Invalid Input: " + ex.Message);
+                return;
+            }
         }
     }
 }
